4959071a628e50e8d964b3181c2ab4f9ba532756
Tryggvi Kalman <tryggvikalman@protonmail.com>
2023-03-07 16:36:16 +0000

    clip trimming fikt, rofi man pages og vivid ls litir

--- .config/nvim/lua/tkj/plugins/treesitter.lua
+++ .config/nvim/lua/tkj/plugins/treesitter.lua
@@ -13,7 +13,7 @@ return {
 				highlight = {
 					enable = true, -- false will disable the whole extension
 					disable = { "latex", "css" }, -- list of language that will be disabled
-					-- additional_vim_regex_highlighting = { "markdown", "rmd" },
+					additional_vim_regex_highlighting = { "markdown", "rmd" },
 				},
 				autopairs = {
 					enable = true,
@@ -21,8 +21,6 @@ return {
 				indent = { enable = true, disable = { "python", "css" } },
 				rainbow = {
 					enable = true,
-					extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-					max_file_lines = nil, -- Do not enable for files with more than n lines, int
 				},
 				matchup = {
 					enable = true, -- mandatory, false will disable the whole extension
@@ -116,4 +114,8 @@ return {
 			})
 		end,
 	},
+	{
+		"HiPhish/nvim-ts-rainbow2",
+		dependencies = { "nvim-treesitter" },
+	}
 }
